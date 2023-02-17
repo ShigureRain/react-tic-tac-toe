5f9904f7fcf70158b5d741e72608dd92e1fbe9b0
Rain <448130604@qq.com>
2023-02-17 17:51:27 +0800

    用循环输出棋盘

--- src/index.js
+++ src/index.js
@@ -2,18 +2,6 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-// class Square extends React.Component {
-//   render() {
-//     return (
-//       <button
-//         className="square"
-//         onClick={() => {this.props.onclick({value: 'X'})}}>
-//         {this.props.value}
-//       </button>
-//     )
-//   }
-// }
-
 const Square = (props) => {
   return (
     <button className="square" onClick={props.onclick}>
@@ -31,58 +19,27 @@ const Board = (props) => {
     )
   }
 
+  const row = [1, 2, 3]
+  let column = 0
+
   return (
     <div>
-      <div className="board-row">
-        {renderSquare(0)}
-        {renderSquare(1)}
-        {renderSquare(2)}
-      </div>
-      <div className="board-row">
-        {renderSquare(3)}
-        {renderSquare(4)}
-        {renderSquare(5)}
-      </div>
-      <div className="board-row">
-        {renderSquare(6)}
-        {renderSquare(7)}
-        {renderSquare(8)}
-      </div>
+      {
+        row.map((key) => {
+          return (
+            <div key={key} className="board-row">
+              {
+                row.map(() => {
+                  return renderSquare(column++)
+                })
+              }
+            </div>
+          )
+        })
+      }
     </div>
   )
 }
-
-// class Board extends React.Component {
-//   renderSquare(i) {
-//     return (
-//       <Square
-//         value={this.props.squares[i]}
-//         onclick={() => {this.props.onClick(i)}}/>
-//     )
-//   }
-//
-//   render() {
-//     return (
-//       <div>
-//         <div className="board-row">
-//           {this.renderSquare(0)}
-//           {this.renderSquare(1)}
-//           {this.renderSquare(2)}
-//         </div>
-//         <div className="board-row">
-//           {this.renderSquare(3)}
-//           {this.renderSquare(4)}
-//           {this.renderSquare(5)}
-//         </div>
-//         <div className="board-row">
-//           {this.renderSquare(6)}
-//           {this.renderSquare(7)}
-//           {this.renderSquare(8)}
-//         </div>
-//       </div>
-//     )
-//   }
-// }
 
 class Game extends React.Component {
   constructor(props) {
