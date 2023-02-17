999b422ad82a1d772e0a583fdcbd4459756240d9
Rain <448130604@qq.com>
2023-02-17 17:09:28 +0800

    展示历史记录坐标

--- src/index.js
+++ src/index.js
@@ -89,7 +89,9 @@ class Game extends React.Component {
     super(props)
     this.state = {
       history: [{
-        squares: Array(9).fill(null)
+        squares: Array(9).fill(null),
+        coordinates: Array(2).fill(null),
+        xIsNext: true
       }],
       stepNumber: 0,
       xIsNext: true
@@ -97,7 +99,7 @@ class Game extends React.Component {
   }
 
   handleClick(i) {
-    const history = this.state.history.slice(0, this.state.stepNumber + 1)
+    const history = this.state.history.slice(0, this.state.stepNumber + 1)  //删除点击的那一项以外多余的记录
     const current = history[history.length - 1]
     const squares = current.squares.slice()
 
@@ -106,6 +108,8 @@ class Game extends React.Component {
     this.setState({
       history: history.concat([{
         squares: squares,
+        coordinates: coordinate(i),
+        xIsNext: this.state.xIsNext,
       }]),
       stepNumber: history.length,
       xIsNext: !this.state.xIsNext
@@ -126,8 +130,10 @@ class Game extends React.Component {
 
     const moves = history.map((step, move) => {
       const desc = move ?
-        'Go to move #' + move :
+        '#' + move + '\n' + (step.xIsNext ? 'X' : 'O') + '\n' + step.coordinates :
         'Go to game start'
+      // 'Go to move #' + move :
+      // 'Go to game start'
       return (
         <li key={move}>
           <button onClick={() => {this.jumpTo(move)}}>{desc}</button>
@@ -181,4 +187,18 @@ const calculateWinner = (squares) => {
   return null
 }
 
+const coordinate = (i) => {
+  const findCoordinate = [
+    [1, 1],
+    [1, 2],
+    [1, 3],
+    [2, 1],
+    [2, 2],
+    [2, 3],
+    [3, 1],
+    [3, 2],
+    [3, 3],
+  ]
+  return findCoordinate[i]
+}
 
